9512982e3f98589e835519e18143d137861b8eaa
beshoyArmia <100165002+beshoyArmia@users.noreply.github.com>
2024-08-17 17:05:06 +0300

    add appbar in noteview page

--- lib/views/noteView.dart
+++ lib/views/noteView.dart
@@ -2,12 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class NoteView extends StatelessWidget {
-
-
   const NoteView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Title'),
+      ),
+      body: Container(),
+    );
   }
 }
