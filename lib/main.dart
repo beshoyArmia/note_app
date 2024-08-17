7e0a56417e7e10870aa98f5ad28c62675deb3efd
beshoyArmia <100165002+beshoyArmia@users.noreply.github.com>
2024-08-17 17:03:17 +0300

    make dart file in views folder

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ void main() {
 }
 
 class NoteApp extends StatelessWidget {
+
   const NoteApp({super.key});
 
   @override
--- lib/views/noteView.dart
+++ lib/views/noteView.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class NoteView extends StatelessWidget {
-  const NoteView({Key? key}) : super(key: key);
+
+
+  const NoteView({super.key});
 
   @override
   Widget build(BuildContext context) {
